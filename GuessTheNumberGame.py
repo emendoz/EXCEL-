26a4b535815c690b5eec3f115863add06c56ea3e
Erika Garnica Mendoza <86032240+emendoz@users.noreply.github.com>
2022-06-12 18:46:01 -0700

    Update GuessTheNumberGame.py
    Cleaned up the code to adjust the print statements original set-up.

--- GuessTheNumberGame.py
+++ GuessTheNumberGame.py
@@ -2,30 +2,30 @@
 import random
 guessesTaken = 0
 
-print("Hello! What is your name?")
+print('Hello! What is your name? ', end = "")
 myName = input()
 
 number = random.randint(1,20)
-print("Well, ", + myName + ", I am thinking of a number between 1 and 20.")
+print('Well, ' + myName + ', I am thinking of a number between 1 and 20.\n')
 
 for i in range(6):
-  print("Take a guess.")
+  print("Take a guess. => ", end = "")
   guess = input()
   guess = int(guess)
   guessesTaken = guessesTaken + 1
   
   if guess < number:
-    print("Your guess is too low.")
+    print("Your guess is too low.\n")
   if guess > number:
-    print("Your guess is too high.")
+    print("Your guess is too high.\n")
   if guess == number:
     break
     
 guessesTaken = str(guessesTaken)
 
 if guess == number:
-  print("Good job," + myName, + "!" + "You guessed my number in " + guessesTaken + "guesses!")
+  print("\nGood job, " + myName + "!" + " You guessed my number in " + guessesTaken + " guesses!")
   
 if guess != number: 
   number = str(number)
-  print("Sorry! The number I was thinking of was " + number + ".")
+  print("\nSorry! The number I was thinking of was " + number + ".")
